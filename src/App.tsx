0376ca3856e31d795b402122d349ca573c3e8a65
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-01-09 13:04:06 -0400

    Configurações iniciais + Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Header from './components/header';
+import './reset.css';
+
 export default function App() {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-[60px]">Hello World</h1>
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <div className="bg-primary flex justify-between items-center p-2">
+      <h1 className="text-white text-[30px]">UnyBay</h1>
+      <ul className="flex gap-5 items-center text-white">
+        <li>Home</li>
+        <li>Quem somos</li>
+        <li>
+          <button className="bg-secondary px-8 py-2 rounded-md">Entrar</button>
+        </li>
+      </ul>
+    </div>
+  );
+
+}
