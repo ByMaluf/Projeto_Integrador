fbd8638f5d7434ad42031fd0beea2851837a0e78
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-01-15 23:20:23 -0400

    Página Login pronta

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,21 @@
 import { useForm } from 'react-hook-form';
 import React from 'react';
+import * as Yup from 'yup'
+import { yupResolver } from "@hookform/resolvers/yup"
 
 type LoginForm = {
   email: string;
   password: string;
 }
 
+const schemaValidation = Yup.object().shape({
+  email: Yup.string().email('Digite um email válido.').required('O campo é obrigatório.'),
+  password: Yup.string().min(4, 'A senha deve ter no mínimo 4 caracteres.').required('O campo é obrigatório.')
+})
+
 export default function Login() {
 
-  const { register, handleSubmit, formState: { errors } } = useForm<LoginForm>();
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginForm>({ resolver: yupResolver(schemaValidation) });
 
   function logar(values: LoginForm) {
     console.log(values);
@@ -24,20 +31,20 @@ export default function Login() {
           <input
             className='w-full border-2 h-[40px] px-2 rounded-md'
             placeholder='Digite seu email'
-            {...register('email', { required: true })}
+            {...register('email')}
             type="text"
           />
-          {errors.email && <span className='text-red-700'>Campo obrigatório</span>}
+          {errors.email && <span className='text-red-700'>{errors.email.message}</span>}
         </React.Fragment>
 
         <React.Fragment>
           <input
             className='w-full border-2 h-[40px] px-2 rounded-md mt-3'
             placeholder='Digite sua senha'
-            {...register('password', { required: true })}
+            {...register('password')}
             type="text"
           />
-          {errors.password && <span className='text-red-700'>Campo obrigatório</span>}
+          {errors.password && <span className='text-red-700'>{errors.password.message}</span>}
         </React.Fragment>
         <button className='rounded-md mt-4 bg-primary w-full h-[40px] text-white' type='submit'>Entrar</button>
       </form>
