712cdffbf10263cb48c4925e1b9f1d3cff7cc87e
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-01-17 21:36:58 -0400

    Link fale conosco

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,13 @@
 import { BsLinkedin, BsFacebook } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer className=" bg-primary text-white py-10 px-10">
-      <h2 className="text-[22px] font-bold mb-5">UnyBay</h2>
+      <div className="flex justify-between items-center">
+        <h2 className="text-[22px] font-bold mb-5">UnyBay</h2>
+        <Link to={'/contact-us'}><p>Fale conosco</p></Link>
+      </div>
       <p className="text-center">Unyleya Educacional | Todos os direitos reservados</p>
       <div className="flex justify-center gap-2 mt-[20px]">
         <BsLinkedin />
